c816da171b81906d8c304bbf4c816f951da9cb23
jhoassirvergara642-dev <jhoassirvergara642@gmail.com>
2026-05-07 13:47:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 const tabla = document.getElementById(“tablaDias”);
 
 // Crear los 31 días automáticamente
-
 for (let i = 1; i <= 31; i++) {
-
     tabla.innerHTML += `
-
-        <tr>
-
+ <tr>
             <td>${i}</td>
-
             <td><input type=“text” id=“tarea${i}”></td>
-
             <td><input type=“checkbox” id=“check${i}”></td>
-
             <td><input type=“text” id=“firma${i}”></td>
-
-        </tr>
-
-    `;
+  </tr>
+ `;
 
 }
 
 function guardarDatos() {
-
     let datos = {};
 
     for (let i = 1; i <= 31; i++) {
-
-        datos[i] = {
-
-            tarea: document.getElementById(tarea${i}).value,
-
-            check: document.getElementById(check${i}).checked,
-
-            firma: document.getElementById(firma${i}).value
+      dato[i] = {
+           tarea: document.getElementById(tarea${i}).value,
+           check: document.getElementById(check${i}).checked,
+           firma: document.getElementById(firma${i}).value
 
         };
 
@@ -47,18 +33,13 @@ function guardarDatos() {
 }
 
 function cargarDatos() {
-
-    let datos = JSON.parse(localStorage.getItem(“limpieza”));
-
-    if (datos) {
+   let datos = JSON.parse(localStorage.getItem(“limpieza”));
+   if (datos) {
 
         for (let i = 1; i <= 31; i++) {
-
-            document.getElementById(tarea${i}).value = datos[i].tarea || “”;
-
-            document.getElementById(check${i}).checked = datos[i].check || false;
-
-            document.getElementById(firma${i}).value = datos[i].firma || “”;
+          document.getElementById(tarea${i}).value = datos[i].tarea || “”;
+          document.getElementById(check${i}).checked = datos[i].check || false;
+          document.getElementById(firma${i}).value = datos[i].firma || “”;
 
         }
 
