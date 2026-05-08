73c7e83ae4641946a0f0fe4b48a6f87054fae523
jhoassirvergara642-dev <jhoassirvergara642@gmail.com>
2026-05-08 12:37:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
-const tabla = document.getElementById(“tablaDias”);
+window.onload = function () {
+const tabla = document.getElementById("tablaDias");
 
-// Crear los 31 días automáticamente
 for (let i = 1; i <= 31; i++) {
-    tabla.innerHTML += `
- <tr>
-            <td>${i}</td>
-            <td><input type=“text” id=“tarea${i}”></td>
-            <td><input type=“checkbox” id=“check${i}”></td>
-            <td><input type=“text” id=“firma${i}”></td>
-  </tr>
- `;
+let fila = document.createElement("tr");
 
-}
+fila.innerHTML = `
+<td>${i}</td>
+<td><input type="text" id="tarea${i}"></td>
+<td><input type="checkbox" id="check${i}"></td>
+<td><input type="text" id="firma${i}"></td>
+`;
 
-function guardarDatos() {
-    let datos = {};
-
-    for (let i = 1; i <= 31; i++) {
-      dato[i] = {
-           tarea: document.getElementById(tarea${i}).value,
-           check: document.getElementById(check${i}).checked,
-           firma: document.getElementById(firma${i}).value
-
-        };
-
-    }
-
-    localStorage.setItem(“limpieza”, JSON.stringify(datos));
-
-    alert(“Datos guardados correctamente”);
-
-}
-
-function cargarDatos() {
-   let datos = JSON.parse(localStorage.getItem(“limpieza”));
-   if (datos) {
-
-        for (let i = 1; i <= 31; i++) {
-          document.getElementById(tarea${i}).value = datos[i].tarea || “”;
-          document.getElementById(check${i}).checked = datos[i].check || false;
-          document.getElementById(firma${i}).value = datos[i].firma || “”;
-
-        }
-
-    }
-
+tabla.appendChild(fila);
 }
 
 cargarDatos();
+};
+
+function guardarDatos() {
+let datos = {};
+
+for (let i = 1; i <= 31; i++) {
+datos[i] = {
+tarea: document.getElementById("tarea" + i).value,
+check: document.getElementById("check" + i).checked,
+firma: document.getElementById("firma" + i).value
+};
+}
+
+localStorage.setItem("limpieza", JSON.stringify(datos));
+alert("Datos guardados");
+}
+
+function cargarDatos() {
+let datos = JSON.parse(localStorage.getItem("limpieza"));
+
+if (datos) {
+for (let i = 1; i <= 31; i++) {
+document.getElementById("tarea" + i).value = datos[i]?.tarea || "";
+document.getElementById("check" + i).checked = datos[i]?.check || false;
+document.getElementById("firma" + i).value = datos[i]?.firma || "";
+}
+}
+}
